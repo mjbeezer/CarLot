fa7942fb3915b9c04cb762e4e6f183732c5cc90b
Matt B <mjbeezer11621@gmail.com>
2021-10-14 14:11:14 -0400

    fourth try

---
+++ GP_CarLot/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GP_CarLot
+{
+    //Parent Class
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+        //constructor
+        public Car(string make, string model, int year, decimal price)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.Price = price;
+        }
+
+        //blank constructor
+        public Car()
+        {
+            
+        }
+    }
+}
---
+++ GP_CarLot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GP_CarLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to The Dream Team Car Lot!");
+        }
+    }
+}
