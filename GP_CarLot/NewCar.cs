295488b8c9236e78690d602cab4eaf8982ad9977
Raston Gilbert <rastong@gmail.com>
2021-10-14 14:41:03 -0400

    New Car class

---
+++ GP_CarLot/NewCar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GP_CarLot
+{
+    class NewCar : Car
+    {
+        public bool IsTrue;
+
+        public NewCar(string make, string model, int year, decimal price, bool isTrue) : base(make, model, year, price)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.Price = price;
+            this.IsTrue = isTrue;
+        }
+
+        public NewCar()
+        {
+            this.Make = "";
+            this.Model = "";
+            this.Year = 0;
+            this.Price = 0;
+            this.IsTrue = true;
+        }
+
+    }
+}
